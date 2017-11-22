bb7a63799e75f405ce0788f94c27bccbe0aa3d0d
Andrew Davey <ajdavey8@gmail.com>
2017-11-22 16:43:33 +0000

    Add some Jquery

---
+++ interface.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  var thermostat = new Thermostat();
+  $('#temperature').text(thermostat.temperature);
+  $('#temperature-up').on('click', function() {
+    thermostat.up();
+    $('#temperature').text(thermostat.temperature);
+  })
+  $('#temperature-down').on('click', function() {
+    thermostat.down();
+    $('#temperature').text(thermostat.temperature);
+  })
+})
